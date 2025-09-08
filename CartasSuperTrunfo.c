3f4f3bbe30ba1a123d66f79a46f053d5cef0a49e
MMaranini <153322991+MMaranini@users.noreply.github.com>
2025-09-08 21:15:44 +0000

    Estrutura de conversa com o usuário

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,6 +18,38 @@ int main() {
     float area1, area2;
     float pib1, pib2;
     int npt1, npt2;
+
+    printf("Carta 1 \n");
+    printf("Digite a primeira letra do estado: ");
+    scanf(" %c", &estado1);
+    printf("Digite o código: ");
+    scanf("%s", cod1);
+    printf("Digite o nome do estado: ");
+    scanf("%s", nome1);
+    printf("Digite a população: ");
+    scanf("%d", &popu1);
+    printf("Digite a área: ");
+    scanf("%f", &area1);
+    printf("Digite o PIB: ");
+    scanf("%f", &pib1);
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &npt1);
+
+    printf("\nCarta 2\n");
+    printf("Digite a primeira letra do estado: ");
+    scanf(" %c", &estado2);
+    printf("Digite o código: ");
+    scanf("%s", cod2);
+    printf("Digite o nome do estado: ");
+    scanf("%s", nome2);
+    printf("Digite a população: ");
+    scanf("%d", &popu2);
+    printf("Digite a área: ");
+    scanf("%f", &area2);
+    printf("Digite o PIB: ");
+    scanf("%f", &pib2);
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &npt2);
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
